5ea2d368a8e1f11bc489ec35ed03e49633b66c0a
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-26 14:54:15 +0300

    Create ArithmeticAverage.java

---
+++ ArithmeticAverage.java
@@ -0,0 +1,10 @@
+public class ArithmeticAverage {
+    public static void main(String[] args) {
+
+
+        double a = 37, b = 68, c = 31;
+        double d = (a + b + c) / 3;
+
+        System.out.println(d);
+    }
+}
